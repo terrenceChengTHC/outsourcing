68c44bf7144a7b8c1c3dfa68aa72ec2ca2773249
terrence <380135745@qq.com>
2016-03-23 22:55:09 +0800

    [function] 1.完成日历控件

--- project160315/test_child_pro/js/index.js
+++ project160315/test_child_pro/js/index.js
@@ -92,4 +92,26 @@ $(function ($) {
 			});
 		})
     });
+	var nowTime = new Date();
+	var maxDate = new Date(nowTime.getFullYear(), nowTime.getMonth(), nowTime.getDate());
+	var childMaxDate = new Date(nowTime.getFullYear(), nowTime.getMonth(), nowTime.getDate()-28);
+	var childMinDate = new Date(nowTime.getFullYear()-10, nowTime.getMonth(), nowTime.getDate());
+	var opt = {
+		preset: 'date', //日期
+		theme: 'android-ics light', //皮肤样式
+		display: 'modal', //显示方式
+		mode: 'scroller', //日期选择模式
+		dateFormat: 'yy-mm-dd', // 日期格式
+		setText: '确定', //确认按钮名称
+		cancelText: '取消',//取消按钮名籍我
+		dateOrder: 'yymmdd', //面板中日期排列格式
+		dayText: '日', monthText: '月', yearText: '年', //面板中年月日文字
+	};
+	$("#parentBirthdayId").mobiscroll(opt).date($.extend(opt,{
+		maxDate: maxDate,
+	}));
+	$("#childBirthdayId").mobiscroll(opt).date($.extend(opt,{
+		maxDate: childMaxDate,
+		minDate: childMinDate
+	}));
 });
